c3513e86a0b314cc4b5bc8645312a1a0c10acda9
Braulio Antonio <antoniohernandezbraulio@gmail.com>
2019-05-24 17:02:02 -0500

    3.9.9
    Login agregado

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'xBadBoy' });
 });
 
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+router.get('/company', function(req, res, next) {
+  res.render('company', { title: 'Company' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Log In' });
+});
+router.get('/sign', function(req, res, next) {
+  res.render('sign', { title: 'Sign up' });
+});
+router.get('/help', function(req, res, next) {
+  res.render('help', { title: 'Help XBadBoy' });
+});
+
 module.exports = router;
